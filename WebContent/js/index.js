05efc4b5729e32be0d565392fa81596a9b29363a
pyjioh <pyjioh@gmail.com>
2016-02-05 21:04:15 +0500

    Add simple Spring RESTful sample

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,6 +1,6 @@
 (function() {
 	var app = angular.module('app', []).controller('ctrl',
 			function($scope, $http) {
-		
+
 			});
 })()
--- src/main/com/example/App.java
+++ src/main/com/example/App.java
@@ -1,18 +1,22 @@
 package main.com.example;
 
-import main.com.example.beans.Data;
-import main.com.example.dao.DataDao;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-		DataDao dataDao = new DataDao();
-		for (Data dataRow : dataDao.getAllData()) {
-			System.out.println(
-				"id: " + dataRow.getId() +
-				" startTime: " + dataRow.getStartTime() +
-				" attendees: " + dataRow.getAttendeeses()
-			);
-		}
+		SpringApplication.run(App.class, args);
+		
+//		DataDao dataDao = new DataDao();
+//		for (Data dataRow : dataDao.getAllData()) {
+//			System.out.println(
+//				"id: " + dataRow.getId() +
+//				" startTime: " + dataRow.getStartTime() +
+//				" attendees: " + dataRow.getAttendeeses()
+//			);
+//		}
+		
 	}
 }
