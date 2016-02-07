281580a39325920aeed5f59b6bd8e9ab38c2357d
pyjioh <pyjioh@gmail.com>
2016-02-07 20:19:16 +0500

    Add generator

--- src/main/com/example/execution/Writer.java
+++ src/main/com/example/execution/Writer.java
@@ -1,44 +1,5 @@
 package main.com.example.execution;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-
-import main.com.example.beans.Attendee;
-import main.com.example.beans.AttendeeId;
-import main.com.example.beans.Event;
-
 public class Writer {
-	private int id;
-	private int startTime;
-	private List<Integer> atendeesNumbers = new LinkedList<>();
 
-	public Writer() {
-
-	}
-
-	private void generateData() {
-		atendeesNumbers.clear();
-		id = new Random().nextInt(10000);
-		startTime = new Random().nextInt(100000) + 1; //can't be 0
-		int atendeesCount = new Random().nextInt(6); // [0..5] values
-		for (int i = 0; i < atendeesCount; i++) {
-			atendeesNumbers.add(i + 2);
-		}
-	}
-	
-	public Event getNewEvent() {
-		generateData();
-		Event event = new Event(id, startTime);
-		
-		Set<Attendee> attendees = new HashSet<Attendee>();
-		for (Integer number : atendeesNumbers) {
-			attendees.add(new Attendee(new AttendeeId(id, number), event));
-		}
-		event.setAttendees(attendees);
-		
-		return event;
-	}
 }
--- src/main/com/example/execution/model/ExecutionModelImpl.java
+++ src/main/com/example/execution/model/ExecutionModelImpl.java
@@ -2,15 +2,15 @@ package main.com.example.execution.model;
 
 import main.com.example.beans.Event;
 import main.com.example.dao.EventDao;
-import main.com.example.execution.Writer;
+import main.com.example.execution.Generator;
 
 public class ExecutionModelImpl implements ExecutionModel {
 	
 	public void generate10() {
-		Writer writer = new Writer();
+		Generator gen = new Generator();
 		EventDao eventDao = new EventDao();
 		for (int i = 0; i < 10; i++) {
-			Event event = writer.getNewEvent();
+			Event event = gen.getNewEvent();
 			eventDao.persist(event);
 		}
 	}
